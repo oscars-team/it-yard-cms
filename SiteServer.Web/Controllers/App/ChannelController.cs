066fc1582002ee672ce8ab6eb991ed2bae68881b
hakunamatata <xymbtc@163.com>
2019-01-15 08:53:00 +0800

    first commit

---
+++ SiteServer.Web/Controllers/App/ChannelController.cs
@@ -0,0 +1,88 @@
+﻿using SiteServer.CMS.Core;
+using SiteServer.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dapper;
+namespace SiteServer.API.Controllers.App
+{
+    internal class Channel
+    {
+        public int cateId { get; set; }
+        public string title { get; set; }
+        public int pid { get; set; }
+        public List<Channel> children { get; set; }
+
+        public Channel()
+        {
+            children = new List<Channel>();
+        }
+    }
+    [RoutePrefix("app/channel")]
+    public class ChannelController : ApiController
+    {
+        Channel[] mappingChannels;
+
+        [HttpGet, Route("")]
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Channel[] channels;
+                using (var conn = SqlUtils.GetIDbConnection(WebConfigUtils.DatabaseType, WebConfigUtils.ConnectionString))
+                {
+                    channels = conn.Query<Channel>("select ChannelName title, Id id, ParentId pid from siteserver_Channel where ParentId=@parentId", new { parentId = id }).ToArray();
+                }
+                return Ok(channels);
+            }
+            catch (Exception ex)
+            {
+                LogUtils.AddErrorLog(ex);
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpGet, Route("tree")]
+        public IHttpActionResult GetTree(int id)
+        {
+            if (id < 1) id = 1;
+            using (var conn = SqlUtils.GetIDbConnection(WebConfigUtils.DatabaseType, WebConfigUtils.ConnectionString))
+            {
+                mappingChannels = conn.Query<Channel>(@"with channel as (
+                                        select Id cateId, ChannelName title, ParentId pid from siteserver_Channel where Id = @id
+                                        union all
+                                        select Id cateId, ChannelName title, ParentId pid from siteserver_Channel c 
+                                        inner join channel c2 on c.ParentId = c2.cateId
+                                        ) select * from channel", new { id }).ToArray();
+            }
+            return Ok(mapChannel(id));
+        }
+
+        private Channel mapChannel(int id)
+        {
+            Channel root;
+            root = new Channel { cateId = id };
+            doMap(root);
+            return root;
+        }
+
+        private void doMap(Channel root)
+        {
+            var children = mappingChannels.Where(p => p.pid == root.cateId).ToList();
+            children.ForEach(e =>
+            {
+                var child = new Channel()
+                {
+                    cateId = e.cateId,
+                    pid = e.pid,
+                    title = e.title
+                };
+                doMap(child);
+                root.children.Add(child);
+            });
+        }
+    }
+}
---
+++ SiteServer.Web/Controllers/App/ContentController.cs
@@ -0,0 +1,60 @@
+﻿using Dapper;
+using SiteServer.CMS.Core;
+using SiteServer.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SiteServer.API.Controllers.App
+{
+    [RoutePrefix("app/content")]
+    public class ContentController : ApiController
+    {
+        [HttpGet, Route("")]
+        public IHttpActionResult Get(int cid, int page = 1, int size = 10)
+        {
+            try
+            {
+                return Ok(getConentsByChannel(cid, page, size));
+            }
+            catch (Exception ex)
+            {
+                LogUtils.AddErrorLog(ex);
+                return InternalServerError(ex);
+            }
+        }
+
+        private dynamic[] getConentsByChannel(int channelId, int page = 1, int size = 10)
+        {
+            dynamic[] contents;
+            using (var conn = SqlUtils.GetIDbConnection(WebConfigUtils.DatabaseType, WebConfigUtils.ConnectionString))
+            {
+                int min = (page - 1) * size;
+                int max = page * size;
+                var sqlQuery = @"select   ct.ChannelId channelId
+                                        , cn.ChannelName channelName
+                                        , ct.Id id
+                                        , Title title
+                                        , ct.AddDate time
+                                        , ct.ImageUrl image
+                                        , VideoUrl video
+                                        , ct.Content content 
+                                        , isTop
+                                        , isHot
+                                        , isColor
+                                        , isRecommend
+                                        from siteserver_Content_1 ct
+                                        inner join siteserver_Channel cn on ct.ChannelId = cn.Id
+                                        where channelId=@channelId";
+                //var sqlCount = $"select count(1) from( {sqlQuery} ) AS t";
+                string order = "order by isTop desc, isHot desc, isColor desc, isRecommend desc";
+                var sqlPagedQuery = $"Select * From (Select ROW_NUMBER() Over ( {order} )  As rowNum, * From ( {sqlQuery} ) As T ) As N Where rowNum > @min And rowNum <= @max";
+                contents = conn.Query(sqlPagedQuery, new { channelId, min, max }).ToArray();
+            }
+            return contents;
+        }
+    }
+}
